7ac871c8acf9ae2ff1379d07f4df15d5f2078557
Naushin khan <naushink2709@gmail.com>
2024-08-03 17:04:52 +0530

    added cpp first file

---
+++ heeloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "CPP Tutorial " << endl;
+
+int a=123;
+cout << a << endl;
+int size=sizeof(a);
+cout << "size of a is "<< size  << endl;
+} 
